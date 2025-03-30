ba48093a6190b29775f591f92c18b63196ac9432
Asif.Khan <asifkhan88012@gmail.com>
2025-03-30 16:21:44 +0530

    Added the entity framework (code first approach): 1- EntityFrameWork SqlServer and Tools added to Infrastructure project. 2- Created a DbContext file 3- Initiated migration and updated database (add-migration init, update database). Note: Set the infrastructure project as start up project

---
+++ Restaurants.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+    internal class RestaurantDbContext : DbContext
+    {
+        internal DbSet<Restaurant> Restaurants { get; set; }    
+
+        internal DbSet<Dish> dishes { get; set; }   
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=NEUDESICB1KJQL3\\SQLEXPRESS;Database=RestaurantDb;Trusted_Connection=True;TrustServerCertificate=True;"); //added TrustServerCertificate to Bypasses SSL/TLS certificate validation when encrypting the connection. Useful when SQL Server is using a self-signed or untrusted certificate.
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Restaurant>()
+                .OwnsOne(r => r.Address);  //address is owned by Restaurant table so we dont need to create seperate table the address columns are present in the Restaurant table
+
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(r => r.Dishes)
+                .WithOne()
+                .HasForeignKey(d => d.RestaurantId);
+        }
+    }
+}
